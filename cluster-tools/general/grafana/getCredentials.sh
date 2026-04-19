f262cb1f49d08acf430ef6f781be22ba5d0cb3ec
Vinicius Pilan <vinipilan@gmail.com>
2026-04-19 19:36:13 -0300

    send metrics from popeye to push gateway

--- cluster-tools/general/grafana/getCredentials.sh
+++ cluster-tools/general/grafana/getCredentials.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-user=$(kubectl get secrets grafana -n metrics -o yaml | yq .data.admin-user | base64 -d)
-password=$(kubectl get secrets grafana -n metrics -o yaml | yq .data.admin-password | base64 -d)
+user=$(kubectl get secrets grafana -n cluster-general-tools -o yaml | yq .data.admin-user | base64 -d)
+password=$(kubectl get secrets grafana -n cluster-general-tools -o yaml | yq .data.admin-password | base64 -d)
 
 echo "Use these credentials to access grafana"
 echo " - user: $user"
 echo " - password: $password"
 echo "Access http://localhost:8081 and use these credentials to access the Grafana Web UI"
 
-kubectl port-forward -n metrics svc/grafana 8081:80
+kubectl port-forward -n cluster-general-tools svc/grafana 8081:80
